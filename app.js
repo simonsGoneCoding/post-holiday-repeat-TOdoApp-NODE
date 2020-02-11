ac2af2f4f7e4cbc955f94696e521cd12adb94e47
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-02-11 21:04:43 +0000

    added functionality for entering and overwriting data

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 
 const command = parseArgs(process.argv);
 delete command._;
-
 // console.log(command);
 
 const handleCommand = ({ add, remove, list }) => {
@@ -18,16 +17,16 @@ const handleCommand = ({ add, remove, list }) => {
         "Error!!! Task name too short... Provide at least 7 characters".red
       );
     }
-    handleData();
+    handleData(1, add);
   } else if (remove) {
-    console.log("removing...");
+    console.log("removing...".green);
     if (typeof remove !== "string" || remove.length < 7) {
       return console.log("Error!!! Task not found".red);
     }
-    handleData();
+    handleData(2, remove);
   } else if (list || list === "") {
-    console.log("printing list...".green);
-    handleData();
+    console.log("preparing to print...".green);
+    handleData(3, list);
   } else {
     console.log(
       "Error!!! Command not recognised...".red +
@@ -45,9 +44,59 @@ const handleCommand = ({ add, remove, list }) => {
 
 const handleData = (type, title) => {
   console.log("handling data...".green);
+  //type - number (1 - add, 2 - remove, 3 - list)
+  //title (string || null)
   const data = fs.readFileSync("db.json");
   const tasks = JSON.parse(data);
-  console.log(typeof tasks);
+  // console.log(tasks);
+  if (type === 1 || type === 2) {
+    const doesExist = tasks.find(task => task.title === title) ? true : false;
+    if (type === 1 && doesExist) {
+      return console.log("Error! Task already added to list!".red);
+    } else if (type === 2 && !doesExist) {
+      return console.log("Error! Task not found!".red);
+    }
+  }
+
+  let dataJSON = "";
+  switch (type) {
+    case 1:
+      const id = tasks.length + 1;
+      tasks.push({ id, title });
+      // console.log(tasks);
+      dataJSON = JSON.stringify(tasks);
+      // console.log(dataJSON);
+      fs.writeFileSync("db.json", dataJSON);
+      console.log(`"${title}" successfully added to list`.black.bgGreen);
+      break;
+
+    case 2:
+      const index = tasks.findIndex(task => task.title === title);
+      tasks.splice(index, 1);
+      dataJSON = JSON.stringify(tasks);
+      fs.writeFile("db.json", dataJSON, "utf8", err => {
+        if (err) throw err;
+        console.log("database updated".green);
+      });
+      console.log(`"${title}" successfully removed from list`.black.bgGreen);
+      break;
+
+    case 3:
+      console.log(`list containing ${tasks.length} tasks as follows:`.green);
+      if (tasks.length) {
+        tasks.forEach((task, index) => {
+          if (index % 2) {
+            console.log(task.title.black.bgGreen);
+          } else {
+            console.log(task.title.black.bgWhite);
+          }
+        });
+        process.exit();
+      } else {
+        console.log("List empty... No tasks to print...".green);
+      }
+      break;
+  }
 };
 
 handleCommand(command);
